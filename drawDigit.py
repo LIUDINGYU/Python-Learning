56cd66a964e885acbcee2a1c25a74c9fc0afcce5
LIUDINGYU <liudy.zju@gmail.com>
2019-05-20 15:33:54 +0800

    修改数码管绘制demo

--- drawDigit.py
+++ drawDigit.py
@@ -1,7 +1,14 @@
 import turtle
+import time
+def DrawGap():   #在单段数码管之间绘制间隔
+    turtle.penup()
+    turtle.fd(5)
+    
 def drawLine(draw):   #绘制单段数码管
+    DrawGap()
     turtle.pendown() if draw else turtle.penup()
     turtle.fd(40)
+    DrawGap()
     turtle.right(90)
 
 def drawDigit(digit):   #根据数字绘制七段数码管
@@ -17,16 +24,28 @@ def drawDigit(digit):   #根据数字绘制七段数码管
     turtle.penup()
     turtle.fd(20)   #为绘制后续数字确定位置
 
-def drawDate(date):   #获得需要输出的数字
+def drawDate(date):   #date为日期，格式设定为'%Y-%m=%d+'，可修改
+    turtle.pencolor("red")
     for i in date:
-        drawDigit(eval(i))
+        if i == '-':   #-标记年的位置，turtle库有直接绘制汉字的方法
+            turtle.write('年',font=("Arial",18,"normal"))
+            turtle.pencolor("green")    #根据年月日改变颜色
+            turtle.fd(40)
+        elif i == '=':
+            turtle.write('月',font=("Arial",18,"normal"))
+            turtle.pencolor("blue")
+            turtle.fd(40)
+        elif i == '+':
+            turtle.write('日',font=("Arial",18,"normal"))
+        else:
+            drawDigit(eval(i))
 
 def main():
     turtle.setup(800,350,200,200)
     turtle.penup()
     turtle.fd(-300)
     turtle.pensize(5)
-    drawDate('20190505')
+    drawDate(time.strftime('%Y-%m=%d+',time.gmtime())) #需要获取系统日期
     turtle.hideturtle()
     turtle.done()
 main()  #调用主函数
